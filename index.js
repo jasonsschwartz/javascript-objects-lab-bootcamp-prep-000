613feda0b70ae21b8e4f7e52d90f958fa92cd5b3
Jason Schwartz <jasonscottschwartz@gmail.com>
2018-08-29 18:07:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = new Object()
+var newObj = new Object()
+
+function updateObjectWithKeyAndValue(object, key, value){
+  //object[key] = value
+  newObj = Object.assign({key: value}, object)
+  return newObj
+}
+
+//function updateObjectWithKeyAndValue(object, key, value){
+//  object[key] = value
+//  return object
+//}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
